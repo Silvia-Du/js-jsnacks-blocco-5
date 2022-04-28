90e84d2994b0c263e225fb861ff2519db7cf59b3
Silvia-Du <silviadu@hotmail.it>
2022-04-28 14:44:36 +0200

    Primo commit: impostazione base di lavoro

---
+++ JSnack 1/script.js
@@ -0,0 +1,34 @@
+/*Jsnack 1
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine.
+*/
+
+const varietàZucchine = [
+  {
+    name: 'zucchina nera',
+    pesoGr: 70,
+    lunghezzaCm: 18,
+  },
+  {
+    name: 'zucchino romanesco',
+    pesoGr: 80,
+    lunghezzaCm: 15,
+  }, 
+  {
+    name: 'zucchino fiorentino',
+    pesoGr: 95,
+    lunghezzaCm: 17,
+  }, 
+  {
+    name: 'zucchina tonda',
+    pesoGr: 70,
+    lunghezzaCm: 15,
+  },
+  {
+    name: 'zucchina trombetta',
+    pesoGr: 75,
+    lunghezzaCm: 18,
+  }
+]
+
+for(let i in varietàZucchine)
---
+++ JSnack 2/script.js
@@ -0,0 +1,14 @@
+/*Jsnack 1
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine.
+Jsnack 2
+Crea 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+--- BONUS
+Jsnack 3
+Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+Jsnack 4
+Crea un array di oggetti, in cui ogni oggetto descrive una bici da corsa con le seguenti proprietà: nome e peso.
+Stampa a schermo la bici con peso minore utilizzando destructuring e template literal
+NB: Se siete indietro con i lavori passati NON lavorate sui bonus ma usate il tempo per sistemarli :faccia_leggermente_sorridente:
+buon lavoro!*/
