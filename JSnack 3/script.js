a8b0356cd48fcafa27867a64f78febcb8fb297c4
Silvia-Du <silviadu@hotmail.it>
2022-04-28 16:17:01 +0200

    Fine Bonus JSnack 3

--- JSnack 3/script.js
+++ JSnack 3/script.js
@@ -5,10 +5,17 @@ Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (
 
 const wordContainer = ['ciao', 'come', 'stai', 'oggi', 'sembra', 'una', 'bella', 'giornata',];
 
+const newLanguage = [];
+
 const wordTurner = (string) => string.split('').reverse().join('');
 
 for(let parola in wordContainer){
 
-  console.log(wordContainer[parola], 'parola');
-  
+  const reverseWord = wordTurner(wordContainer[parola])
+  newLanguage.push(reverseWord);
 }
+
+console.log(newLanguage);
+
+document.getElementById('output-1').innerHTML = `La frase iniziale : " ${wordContainer.join(' ')}! "`;
+document.getElementById('output-2').innerHTML = `Nella nuova lingua improponibilie : " ${newLanguage.join(' ')}! "`;
