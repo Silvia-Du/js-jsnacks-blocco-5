dca28211854b1d891365b2d784f82115916078e7
Silvia-Du <silviadu@hotmail.it>
2022-04-28 16:27:35 +0200

    Inizio JSnack 4

--- JSnack 4/script.js
+++ JSnack 4/script.js
@@ -1,14 +1,41 @@
-/*Jsnack 1
-Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
-Calcola quanto pesano tutte le zucchine.
-Jsnack 2
-Crea 10 oggetti che rappresentano una zucchina.
-Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
---- BONUS
-Jsnack 3
-Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+/*
 Jsnack 4
 Crea un array di oggetti, in cui ogni oggetto descrive una bici da corsa con le seguenti proprietà: nome e peso.
 Stampa a schermo la bici con peso minore utilizzando destructuring e template literal
-NB: Se siete indietro con i lavori passati NON lavorate sui bonus ma usate il tempo per sistemarli :faccia_leggermente_sorridente:
-buon lavoro!*/
+*/
+
+
+const bikeContainer = [
+  {
+    name: 'Scott Scale98',
+    weightKg: '12',
+  },
+  {
+    name: 'Orbea mx50',
+    weightKg: '12.5',
+  }, 
+  {
+    name: 'Conor-WRC',
+    weightKg: '13',
+  }, 
+  {
+    name: 'Scott Aspect',
+    weightKg: '14',
+  }, 
+  {
+    name: 'Graziella',
+    weightKg: '17',
+  },
+  {
+    name: 'Spark RC',
+    weightKg: '12',
+  },
+  {
+    name: 'Scott Ransom',
+    weightKg: '12.5',
+  },
+  {
+    name: 'Scott Volage',
+    weightKg: '11',
+  }
+]
